e3a8cd901da5299a8952f5e45e29acc537748908
Mirtek <mirekpesta@gmail.com>
2024-04-19 14:42:32 +0200

    ....

---
+++ Python_Samples/05-Astrologer/sample.py
@@ -0,0 +1,27 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      SESA237770
+#
+# Created:     18.04.2024
+# Copyright:   (c) SESA237770 2024 import swisseph as swe
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+##import swisseph as swe
+##from kerykeion import AstrologicalSubject
+
+import sys
+
+
+##=======================================================
+if __name__ == '__main__':
+
+    print("===========================================================")
+    print("=====ASTROLOGY SANDBOX=====================================")
+    print("===========================================================")
+
+    print(sys.path)
+
+##    kanye = AstrologicalSubject("Kanye", 1977, 6, 8, 8, 45, "Atlanta")
